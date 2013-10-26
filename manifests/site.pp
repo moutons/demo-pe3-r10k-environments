7b17a9bb29ad3aa8367ed10e8006d6a4ee717b70
Shaun Mouton <shaun.mouton@austintexas.gov>
2013-10-26 15:21:42 -0500

    adding bind server conf

--- manifests/site.pp
+++ manifests/site.pp
@@ -31,6 +31,22 @@ node /^agent0.*/ inherits base {
 
 node /^agent1.*/ inherits base {
   include bind
+  bind::server::conf { '/etc/named.conf':
+    listen_on_addr    => [ 'any' ],
+    listen_on_v6_addr => [ 'any' ],
+    forwarders        => [ '8.8.8.8', '8.8.4.4' ],
+    allow_query       => [ 'localnets' ],
+    zones             => {
+      'myzone.lan' => [
+        'type master',
+        'file "myzone.lan"',
+      ],
+      '1.168.192.in-addr.arpa' => [
+        'type master',
+        'file "1.168.192.in-addr.arpa"',
+      ],
+    },
+  }
   notify { "${::hostname} classified as agent1.": }
 }
 
